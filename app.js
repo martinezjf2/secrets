e657bd4d8b22421154b5901e6ac3b4962db3cdee
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-31 18:13:47 -0500

    adding google oauth2.0 and mongoose findOrCreate package which is not a mongoose or mongodb method, set up the callback for google oauth and set up the plugin for findOrCreate

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20')
+const findOrCreate = require('mongoose-findorcreate')
 // const bcrypt = require("bcrypt")
 // const saltRounds = 10;
 // const md5 = require('md5')
@@ -50,6 +52,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // const secret = process.env.SECRET
 // userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
@@ -62,6 +65,19 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.use(bodyParser.urlencoded({extended: true}))
 
