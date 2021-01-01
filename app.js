ae096c7b049e081fe5eee8dd8a8fb44f73582a59
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-01 01:46:31 -0500

    add post to submit a new secret

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true, 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -134,11 +135,15 @@ app.get('/register', function(req,res) {
 });
 
 app.get('/secrets', function(req, res){
-    if (req.isAuthenticated()) {
-        res.render("secrets")
-    } else {
-        res.render("login")
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    })
 })
 
 app.post("/register", function(req,res) {
@@ -159,6 +164,31 @@ app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/")
 })
+
+app.get('/submit', function(req, res){
+    if (req.isAuthenticated()) {
+        res.render("submit")
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.post('/submit', function(req, res){
+    const submittedSecret = req.body.secret
+    User.findById(req.user.id, function(err, foundUser){
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect('/secrets')
+                })
+            }
+        }
+    })
+
+});
 
 app.listen(3000, function(){
     console.log('You are listening to Port:3000');
